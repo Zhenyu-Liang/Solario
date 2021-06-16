45f81f45359c3e8218d5ab88400907471aac20b8
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-06-16 09:17:19 -0400

    removed the flask app and will use firebase

--- public/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-
-class ContactInfo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    email = db.Column(db.String(255))
-    message = db.Column(db.String(1000))
