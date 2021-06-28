89fc386f12ec3338a59ed1cfb87d101c00fb9f17
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-06-28 14:31:59 -0400

    start working on the planets page

--- public/js/redirect.js
+++ public/js/redirect.js
@@ -14,20 +14,26 @@ for (let item of gamePageLinks) {
 	item.setAttribute("href", window.location.protocol + "//" + window.location.host + "/game/");
 }
 
+
 /* Button Redirect */
 
 let exploreBtn = document.getElementById("explore-btn");
 let homeDownBtn = document.getElementById("home-down-btn");
 let newsBtn = document.getElementById("learn-more-news-btn");
+let planetsBtn = document.getElementById("learn-more-planets-btn");
 
 exploreBtn.addEventListener("click", () => {
 	window.location.href = "#planets";
 })
 
 homeDownBtn.addEventListener("click", () => {
-	window.location.href = "#planets"
+	window.location.href = "#planets";
 });
 
 newsBtn.addEventListener("click", () => {
-	window.location.href = "https://www.space.com/"
+	window.location.href = "https://www.space.com/";
+})
+
+planetsBtn.addEventListener("click", () => {
+	window.location.href = window.location.protocol + "//" + window.location.host + "/planets";
 })
