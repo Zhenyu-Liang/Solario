e80d7fac5375e49894dcb5435aec8fa33d795b55
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-07-02 15:10:26 -0400

    appended all images for the planets to a object in data.js

--- public/planets/js/data.js
+++ public/planets/js/data.js
@@ -1,3 +1,56 @@
 const planetsData = {
 
+	"sun": {
+		title: "Sun",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/sun.png"
+	},
+
+	"mercury": {
+		title: "Mercury",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/mercury.png"
+	},
+
+	"venus": {
+		title: "Venus",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/venus.png"
+	},
+
+	"earth": {
+		title: "Earth",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/earth.png"
+	},
+
+	"mars": {
+		title: "Mars",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/mars.png"
+	},
+
+	"jupiter": {
+		title: "Jupiter",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/jupiter.png"
+	},
+
+	"saturn": {
+		title: "Saturn",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/saturn.png"
+	},
+
+	"uranus": {
+		title: "Uranus",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/uranus.png"
+	},
+
+	"neptune": {
+		title: "Neptune",
+		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		img: "../img/planets_imgs/neptune.png"
+	}
 };
