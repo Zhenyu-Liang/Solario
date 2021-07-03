aceffac15f90accd54da47ac18755765b3ad302b
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2021-07-03 17:09:17 -0400

    added all informations for the planets, work on the solar system simulation next

--- public/planets/js/data.js
+++ public/planets/js/data.js
@@ -14,43 +14,43 @@ const planetsData = {
 
 	"venus": {
 		title: "Venus",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Venus, the second planet from the Sun and it is the sixth-largest and the hottest planet in our solar system. Its surface temperature is 462 degrees Celsius and it has no moon. Its orbit period is 224,70 Earth days. ",
 		img: "img/planets_imgs/venus.png"
 	},
 
 	"earth": {
 		title: "Earth",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Earth, our home planet, is the third planet from the Sun and it is the fifth-largest planet in the solar system. It is currently the planet in the universe where we've ever identified life. It takes Earth 23.934 hours to complete a rotation on its axis and 365.26 days to complete an orbit around sun.",
 		img: "img/planets_imgs/earth.png"
 	},
 
 	"mars": {
 		title: "Mars",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Mars, the fourth planet from Sun. It is one of the most explored bodies in our solar system. It is a dusty, cold, desert, world with a very thin atmosphere. The size of the planet is about half the size of Earth. A day on Mars is 24.6 hours and a year on Mars is 687 Earth days. ",
 		img: "img/planets_imgs/mars.png"
 	},
 
 	"jupiter": {
 		title: "Jupiter",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Jupiter, it is the fifth planet from the sun and it is the largest planet in the solar system. More than 1,300 Earths would fit inside Jupiter. The planet is a giant ball of gas and it is extremely windy. A day on Jupiter is about 10 hours long and a year on Jupiter is 12 years on Earth.",
 		img: "img/planets_imgs/jupiter.png"
 	},
 
 	"saturn": {
 		title: "Saturn",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Saturn, the sixth planet from the Sun and the second largest planet in our solar system. It has ring and the ring isn't solid. You also can't stand in Saturn and saturn is mostly made of gas. A day on Saturn is 10 hours and 42 minutes on Earth and a year on Saturn is 29 years on Earth. ",
 		img: "img/planets_imgs/saturn.png"
 	},
 
 	"uranus": {
 		title: "Uranus",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Uranus, the seventh planet from the Sun and the third largest planet in our solar system. It is a very cold and windy planet. It rotates at a nearly 90 degree angle from the plane of its orbit. A day on Uranus is about 17 hours on Earth and a year on Uranus is about 84 Earth years.",
 		img: "img/planets_imgs/uranus.png"
 	},
 
 	"neptune": {
 		title: "Neptune",
-		information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+		information: "Neptune, the eighth planet from the Sun and it is the farest planet from the Sun. It is a ice giant type planet and it has 14 moons. One day on Neptune is 16 hours on Earth and one year on Neptune is 165 Earth years." ,
 		img: "img/planets_imgs/neptune.png"
 	}
 };
